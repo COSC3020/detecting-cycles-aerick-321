f34e33ee10381e72afaa1e653ef74b5a5c2681f8
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-12-01 14:24:04 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 function hasCycle(graph) {
+    let minSpTree = new Set();
+    let edge;
+    for (let node = 0; node - 1> edge; node++){
+        if(!minSpTree.has(cycle)){
+            minSpTree.add(edge)
+            return true;
+        }
+        return false;
+    }
     return false;
+}
+
+function detectCycle(){
+    
 }
