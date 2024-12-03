174e605224ed5bd061e5baf7f0361fa543ab02fb
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-12-03 16:12:36 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,32 +1,29 @@
 function hasCycle(graph) {
     let visited = new Set();
+    
+    function detectCycle(current, parent, graph) {
+        visited.add(current);
+
+        for (let next of graph[current]) {
+            if (!visited.has(next)) {
+                if (detectCycle(next, current, graph)) {
+                    return true;
+                }
+            } else if (next !== parent) { 
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     for (let node in graph) {
         if (!visited.has(node)) {
             if (detectCycle(node, null, graph)) {
-                return true; 
+                return true;
             }
         }
     }
-    return false;
-    let stack = new Set();
-    
-    function detectCycle(current, parent, graph){
-        visited.add(current);
-        stack.add(current);
-    
-        for (let next in graph[current]) {
-            if(!visited.has(next)){
-                stack.push(current);
-                if (detectCycle(next, current, graph) {
-                return true; 
-                }
-                
-            } else if (stack.has(next)) {
-            return true;
-            }
-        }
-        return false;
-    }
-}
 
+    return false;
+}
